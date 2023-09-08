b61d06eb41d28feb7cd6c4d0a52a5fed4b611ceb
Raxan7 <manyerere201@gmail.com>
2023-09-09 02:03:23 +0300

    Added admin settings for managing stock

--- hotelmanagement/admin.py
+++ hotelmanagement/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Reservation, Restaurant, Review, MenuCategory, MenuItem, Order, OrderItem, Employee, Payment
+from .models import Reservation, Restaurant, Review, MenuCategory, MenuItem, Order, OrderItem, Employee, Payment, Stock
 # Register your models here.
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'expiration_date', 'is_low_inventory')
+    list_filter = ('is_low_inventory',)
+    search_fields = ('name',)
+
 
 admin.site.register(Reservation)
 admin.site.register(Restaurant)
