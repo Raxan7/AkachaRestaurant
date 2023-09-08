caa91487a3b3aebc515eee02752257122975a6a7
Raxan7 <manyerere201@gmail.com>
2023-09-09 02:02:09 +0300

    Created tables for storing stock inventory and ran migrations

--- hotelmanagement/models.py
+++ hotelmanagement/models.py
@@ -7,6 +7,20 @@ class CustomUser(AbstractUser):
     id = models.AutoField(primary_key=True)
 
 
+class Stock(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.PositiveIntegerField()
+    expiration_date = models.DateField()
+    reorder_quantity = models.PositiveIntegerField(default=10)  # Set your desired reorder threshold here
+
+    def is_low_inventory(self):
+        return self.quantity < self.reorder_quantity
+
+    def __str__(self):
+        return self.name
+
+
 class Restaurant(models.Model):
     objects = models.Manager()
     id = models.AutoField(primary_key=True)
