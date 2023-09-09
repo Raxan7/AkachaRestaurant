c9b455f00cd761f83f671218cc04fb4e8f5c4e8f
Raxan7 <manyerere201@gmail.com>
2023-09-09 15:35:04 +0300

    Modified views.py

--- StockManagerApp/views.py
+++ StockManagerApp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 
-from AkachaRestaurant.StockManagerApp.forms import StockForm
-from AkachaRestaurant.StockManagerApp.models import Stock
+from .forms import StockForm
+from .models import Stock
 
 
 def food_item_list(request):
     food_items = Stock.objects.all()
-    return render(request, 'hotelmanagement/food_item_list.html', {'food_items': food_items})
+    return render(request, 'StockManagerApp/food_item_list.html', {'food_items': food_items})
 
 
 def add_food_item(request):
@@ -17,7 +17,7 @@ def add_food_item(request):
             return redirect('food_item_list')
     else:
         form = StockForm()
-    return render(request, 'hotelmanagement/add_food_item.html', {'form': form})
+    return render(request, 'StockManagerApp/add_food_item.html', {'form': form})
 
 
 def edit_food_item(request, pk):
@@ -29,10 +29,10 @@ def edit_food_item(request, pk):
             return redirect('food_item_list')
     else:
         form = StockForm(instance=food_item)
-    return render(request, 'hotelmanagement/edit_food_item.html', {'form': form, 'food_item': food_item})
+    return render(request, 'StockManagerApp/edit_food_item.html', {'form': form, 'food_item': food_item})
 
 
 def view_food_item(request, pk):
     food_item = Stock.objects.get(pk=pk)
-    return render(request, 'hotelmanagement/view_food_item.html', {'food_item': food_item})
+    return render(request, 'StockManagerApp/view_food_item.html', {'food_item': food_item})
 
