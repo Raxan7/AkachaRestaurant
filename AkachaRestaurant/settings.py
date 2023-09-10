8294364b330eb8fb453f1460e3cf832795e6b6b4
Raxan7 <manyerere201@gmail.com>
2023-09-10 11:33:47 +0300

    Modified whitenoise configurations

--- AkachaRestaurant/settings.py
+++ AkachaRestaurant/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
 # ]
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
