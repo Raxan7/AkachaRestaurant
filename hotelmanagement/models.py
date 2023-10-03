9e58e4da94839b20348536ab3f51e6dba04b7e77
Raxan7 <manyerere201@gmail.com>
2023-10-03 22:16:48 +0300

    Modified some files to enable notification handling

--- hotelmanagement/models.py
+++ hotelmanagement/models.py
@@ -6,12 +6,14 @@ from django.contrib.auth.models import AbstractUser
 class User_type(models.Model):
     id = models.AutoField(primary_key=True)
     user_type = models.CharField(max_length=50)
+
     def __str__(self):
         return self.user_type
-    
+
+
 class CustomUser(AbstractUser):
     id = models.AutoField(primary_key=True)
-    profile = models.ImageField(upload_to="profiles/",  default='profiles/default_profile.jpg')
+    profile = models.ImageField(upload_to="profiles/", default='profiles/default_profile.jpg')
     user_type = models.ForeignKey(User_type, on_delete=models.SET_NULL, null=True, default=None)
 
     def __str__(self):
@@ -26,6 +28,7 @@ class Restaurant(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     description = models.TextField()
 
+
 class Table(models.Model):
     id = models.AutoField(primary_key=True)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
@@ -37,6 +40,7 @@ class MenuCategory(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
+
 class MenuItem(models.Model):
     id = models.AutoField(primary_key=True)
     category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
@@ -44,11 +48,13 @@ class MenuItem(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
+
 class MenuImage(models.Model):
     id = models.AutoField(primary_key=True)
     image = models.ImageField(upload_to="menus/")
     menu_category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+
 
 class Reservation(models.Model):
     id = models.AutoField(primary_key=True)
@@ -58,6 +64,7 @@ class Reservation(models.Model):
     reservation_date = models.DateTimeField()
     party_size = models.PositiveIntegerField()
 
+
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
@@ -65,11 +72,13 @@ class Order(models.Model):
     menu_items = models.ManyToManyField(MenuItem, through='OrderItem')
     order_date = models.DateTimeField()
 
+
 class OrderItem(models.Model):
     id = models.AutoField(primary_key=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
@@ -78,6 +87,7 @@ class Review(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     comment = models.TextField()
     date = models.DateTimeField()
+
 
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
@@ -88,9 +98,18 @@ class Employee(models.Model):
     role = models.CharField(max_length=255)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
 
+
 class Payment(models.Model):
     id = models.AutoField(primary_key=True)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField()
     payment_method = models.CharField(max_length=255)
+
+
+class Messages(models.Model):
+    sender = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE)
+    time_sent = models.TimeField(auto_now=True)
+    opened = models.BooleanField(default=False)
+    message = models.TextField()
