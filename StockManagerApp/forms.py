79968fff97abf3ed8ed54b620167b05dc17b3273
Raxan7 <manyerere201@gmail.com>
2023-10-10 16:29:51 +0300

    Modified the StockManagerApp

--- StockManagerApp/forms.py
+++ StockManagerApp/forms.py
@@ -1,10 +1,25 @@
 # forms.py
 
 from django import forms
-from .models import Stock
+from .models import Stock, StockPurchases, RequestStock
 
 
 class StockForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ['name', 'description', 'quantity', 'expiration_date', 'reorder_quantity']
+
+
+class PurchasesForm(forms.ModelForm):
+    class Meta:
+        model = StockPurchases
+        fields = ['seller_name', 'goods_bought', 'price', ]
+
+
+class RequestFoodFromStore(forms.ModelForm):
+
+    quantity = forms.IntegerField(required=True)
+
+    class Meta:
+        model = RequestStock
+        fields = ['name']
--- StockManagerApp/urls.py
+++ StockManagerApp/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('food_items/add/', views.add_food_item, name='add_food_item'),
     path('food_items/<int:pk>/', views.view_food_item, name='view_food_item'),
     path('food_items/<int:pk>/edit/', views.edit_food_item, name='edit_food_item'),
+    path('manage_sales/', views.manage_sales, name='manage_sales'),
+    path('register_purchases/', views.purchase_registration, name='purchases_register'),
+    path('purchases_list/', views.purchases_list_view, name='purchases_list'),
+    path('purchases_detail/<int:pk>', views.purchases_detail, name='view_purchase_item'),
+    path('request_foods/', views.request_food_items_view, name='request_food_items'),
 ]
