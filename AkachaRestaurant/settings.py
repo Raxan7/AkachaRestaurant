7cd2f3be8a19209f2617361babd18f5cc0646498
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-10-18 12:27:34 +0300

    some changes

--- AkachaRestaurant/settings.py
+++ AkachaRestaurant/settings.py
@@ -85,17 +85,17 @@ WSGI_APPLICATION = 'AkachaRestaurant.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     "default": dj_database_url.parse(env("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    "default": dj_database_url.parse(env("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -129,12 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# if 'DJANGO_SETTINGS_MODULE' in os.environ:
-#     # Production settings
-#     STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
-# else:
-#     # Development settings
-STATICFILES_DIRS = [
+if 'DJANGO_SETTINGS_MODULE' in os.environ:
+    # Production settings
+    STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
+else:
+    # Development settings
+    STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
     
