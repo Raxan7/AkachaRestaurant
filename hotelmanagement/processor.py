c8a8919626987ca66b0c36077ad9500b57810479
Raxan7 <manyerere201@gmail.com>
2023-10-25 11:45:39 +0300

    Added a feature for displaying messages for the Storekeeper

--- hotelmanagement/processor.py
+++ hotelmanagement/processor.py
@@ -7,9 +7,14 @@ def user_type_list(request):
 
 def message_analyser(request):
     try:
-        user = request.user
-        messages = Messages.objects.filter(receiver_category=user.user_type, order__send=False)
-        return {'messagess':messages}
+        if request.user.user_type.user_type == "Storekeeper":
+            print(request.user.user_type)
+            sys_messages = Messages.objects.filter(receiver_category=2, opened=False)
+            return {"system_message": sys_messages}
+        else:
+            user = request.user
+            messages = Messages.objects.filter(receiver_category=user.user_type, order__send=False)
+            return {'messagess': messages}
     except:
         
        user_type = User_type.objects.all()
