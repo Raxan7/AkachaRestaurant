a069f29f7218c7e46152b492c9847375441eae37
Raxan7 <manyerere201@gmail.com>
2023-10-25 17:50:37 +0300

    Added messaging feature for Storekeeper

--- hotelmanagement/processor.py
+++ hotelmanagement/processor.py
@@ -1,9 +1,11 @@
 from .models import User_type, Messages
 from django.shortcuts import render, redirect
 
+
 def user_type_list(request):
     user_type = User_type.objects.all()
     return {'user_types': user_type}
+
 
 def message_analyser(request):
     try:
@@ -16,6 +18,6 @@ def message_analyser(request):
             messages = Messages.objects.filter(receiver_category=user.user_type, order__send=False)
             return {'messagess': messages}
     except:
-        
-       user_type = User_type.objects.all()
-       return {'user_types': user_type}
+
+        user_type = User_type.objects.all()
+        return {'user_types': user_type}
