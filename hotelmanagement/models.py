25313f55a13cd522aa7f20f308d58dc7a1601c52
Raxan7 <manyerere201@gmail.com>
2023-11-28 17:28:30 +0300

    Added objects manager

--- hotelmanagement/models.py
+++ hotelmanagement/models.py
@@ -8,7 +8,6 @@ from django.db.models import Sum
 
 # Create your models here.
 class User_type(models.Model):
-    
     id = models.AutoField(primary_key=True)
     user_type = models.CharField(max_length=50)
 
@@ -17,61 +16,67 @@ class User_type(models.Model):
 
 
 class CustomUser(AbstractUser):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     profile = models.ImageField(upload_to="profiles/", default='profiles/default_profile.jpg')
     user_type = models.ForeignKey(User_type, on_delete=models.SET_NULL, null=True, default=None)
     is_active = models.BooleanField(default=True)
     customer_profit = models.FloatField(default=0.0)
+
     def update_customer_profit(instance):
         order = instance
-        total_profit = Order.objects.filter(order_receiver=order).aggregate(Sum('menu_items__item_profit'))['menu_items__item_profit__sum'] or 0
+        total_profit = Order.objects.filter(order_receiver=order).aggregate(Sum('menu_items__item_profit'))[
+                           'menu_items__item_profit__sum'] or 0
         order.customer_profit = total_profit
         order.save()
-    
 
 
 class Restaurant(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     contact = models.CharField(max_length=255)
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     description = models.TextField()
-    
 
 
 class Table(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     # restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     table_number = models.CharField(max_length=10)
     capacity = models.PositiveIntegerField()
-    
 
 
 class MenuCategory(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    
 
 
 class MenuItem(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.FloatField()
     average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
-    ingredient_cost = models.FloatField( default=0)
+    ingredient_cost = models.FloatField(default=0)
     item_profit = models.FloatField(default=0)
     orders_number = models.FloatField(default=0)
+
     def save(self, *args, **kwargs):
         self.item_profit = self.price - self.ingredient_cost
         super(MenuItem, self).save(*args, **kwargs)
+
     def update_cost(instance):
         menu_item = instance
         total_cost = Ingredient.objects.filter(menu_item=menu_item).aggregate(Sum('price'))['price__sum'] or 0
         menu_item.ingredient_cost = total_cost
         menu_item.save()
+
     def update_orders_number(instance):
         menu_item = instance
         total_number = Order.objects.filter(menu_items=menu_item).count()
@@ -80,32 +85,33 @@ class MenuItem(models.Model):
 
 
 class MenuItemRating(models.Model):
+    objects = models.Manager()
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  # Link the rating to a user
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     rating = models.DecimalField(decimal_places=1, max_digits=2)
     comment = models.TextField(max_length=200, default="The food is too good")
-    
 
 
 class MenuImage(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
-    image = models.ImageField(upload_to = "menus/")
+    image = models.ImageField(upload_to="menus/")
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     is_primary = models.BooleanField(default=False)
-    
 
 
 class Reservation(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     guest_name = models.CharField(max_length=255)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     reservation_date = models.DateTimeField()
     party_size = models.PositiveIntegerField()
-    
 
 
 class Order(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     # restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
@@ -117,37 +123,38 @@ class Order(models.Model):
     order_processor = models.ForeignKey(CustomUser, related_name="processor", on_delete=models.DO_NOTHING, null=True)
     order_receiver = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, null=True)
     send = models.BooleanField(default=False)
-    
 
 
 class OrderItem(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    
 
 
 class Review(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     guest_name = models.CharField(max_length=255)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     rating = models.DecimalField(max_digits=3, decimal_places=2)
     comment = models.TextField()
     date = models.DateTimeField()
-    
-    
+
+
 class Ingredient(models.Model):
-    id = models.AutoField(primary_key = True)
-    menu_item = models.ForeignKey(MenuItem, on_delete = models.CASCADE)
+    objects = models.Manager()
+    id = models.AutoField(primary_key=True)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient_name = models.CharField(max_length=50)
     measured_in = models.CharField(max_length=50)
     quantity = models.FloatField()
     price = models.FloatField()
-    
-    
-    
+
+
 class Employee(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     email = models.EmailField()
@@ -155,16 +162,15 @@ class Employee(models.Model):
     address = models.TextField()
     role = models.CharField(max_length=255)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
-    
 
 
 class Payment(models.Model):
+    objects = models.Manager()
     id = models.AutoField(primary_key=True)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField()
     payment_method = models.CharField(max_length=255)
-    
 
 
 class Messages(models.Model):
@@ -185,7 +191,6 @@ class Messages(models.Model):
         ("None", "None")
     )
 
-    
     sender = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE, related_name="message_sender")
     receiver_category = models.ForeignKey(User_type, on_delete=models.CASCADE, null=True)
     # receiver = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE, related_name="message_receiver")
@@ -200,15 +205,18 @@ class Messages(models.Model):
     def __str__(self):
         return f"{self.message}"
 
+
 @receiver(post_save, sender=Ingredient)
 @receiver(post_delete, sender=Ingredient)
 def update_menu_item(sender, instance, **kwargs):
     instance.menu_item.update_cost()
 
+
 @receiver(post_save, sender=Order)
 def update_menu_item(sender, instance, **kwargs):
     instance.order_receiver.update_customer_profit()
-    
+
+
 @receiver(post_save, sender=Order)
 @receiver(post_delete, sender=Order)
 def update_menu_item(sender, instance, **kwargs):
