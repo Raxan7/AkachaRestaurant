f40ed5ed8f1feec1ed7b31e4403946aefc87541c
Raxan7 <manyerere201@gmail.com>
2023-11-30 11:59:53 +0300

    Updated the static settings

--- AkachaRestaurant/settings.py
+++ AkachaRestaurant/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
 STATIC_URL = '/static/'
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
