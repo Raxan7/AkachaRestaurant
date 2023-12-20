d8a97f018ddf935f0890df4229421133bb8cfda8
Raxan7 <manyerere201@gmail.com>
2023-12-20 11:59:42 +0300

    Updated build.sh

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-DJANGO_SUPERUSER_EMAIL="atmaiwiz@gmail.com" \
-DJANGO_SUPERUSER_FIRST_NAME="Anania" \
-DJANGO_SUPERUSER_MIDDLE_NAME="Tenson" \
-DJANGO_SUPERUSER_LAST_NAME="Mtawa" \
-DJANGO_SUPERUSER_PASSWORD="1234" \
-python3 manage.py createsuperuser --username atmaiwiz@gmail.com --noinput 
+# DJANGO_SUPERUSER_EMAIL="atmaiwiz@gmail.com" \
+# DJANGO_SUPERUSER_FIRST_NAME="Anania" \
+# DJANGO_SUPERUSER_MIDDLE_NAME="Tenson" \
+# DJANGO_SUPERUSER_LAST_NAME="Mtawa" \
+# DJANGO_SUPERUSER_PASSWORD="1234" \
+# python3 manage.py createsuperuser --username atmaiwiz@gmail.com --noinput 
 
 python3 manage.py collectstatic --no-input
 
