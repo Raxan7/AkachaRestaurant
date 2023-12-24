86f1d1fa38d791eea6fedcbb3da2091d8de9fc71
Raxan7 <manyerere201@gmail.com>
2023-12-24 11:23:39 +0300

    Added an app in installed apps

--- AkachaRestaurant/settings.py
+++ AkachaRestaurant/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-# import dj_database_url
-# import environ
+import dj_database_url
+import environ
 
-# env = environ.Env()
+env = environ.Env()
 
-# environ.Env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     'hotelmanagement',
     'StockManagerApp',
+    'EmployeeManagement',
 
     # 'widget_tweaks',
 ]
@@ -132,11 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
-# STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
 STATIC_URL = '/static/'
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
