6e29070b557c0ccd76e8ecd82591b6b792e22eb1
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-12-30 00:16:22 +0300

    admin

--- EmployeeManagement/admin.py
+++ EmployeeManagement/admin.py
@@ -5,11 +5,11 @@ from .models import *
 def get_model_fields(self, model):
     return [field.name for field in model._meta.fields]    
 
-class EmployeeDetailsAdmin(admin.ModelAdmin):
-    list_display = get_model_fields(EmployeeDetails)
-    search_fields = get_model_fields(EmployeeDetails)
-    # list_filter = ('')
-    ordering = get_model_fields(EmployeeDetails)
-    # readonly_fields = ('')
-admin.site.register(EmployeeDetails, EmployeeDetailsAdmin)
+# class EmployeeDetailsAdmin(admin.ModelAdmin):
+#     list_display = get_model_fields(EmployeeDetails)
+#     search_fields = get_model_fields(EmployeeDetails)
+#     # list_filter = ('')
+#     ordering = get_model_fields(EmployeeDetails)
+#     # readonly_fields = ('')
+# admin.site.register(EmployeeDetails, EmployeeDetailsAdmin)
 
