2731f1fa57459fe1d171aa151628d1d4278c3430
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-12-29 23:35:29 +0300

    admin modifications

--- EmployeeManagement/admin.py
+++ EmployeeManagement/admin.py
@@ -3,19 +3,9 @@ from django.contrib import admin
 from .models import EmployeeDetails
 
 class EmployeeDetailsAdmin(admin.ModelAdmin):
-    list_display = ('employeeID', 'social_security_number', 'gender', 'date_of_birth','National_id_number')  # Fields to be displayed in the list view
-    search_fields = ('employeeID', 'social_security_number','National_id_number')  # Enable searching by these fields
-    list_filter = ('employeeID','National_id_number')  # Add filters for these fields
-    ordering = ('employeeID','social_security_number')  # Set the default sorting order
-
-    fieldsets = (
-        ('Main Information', {
-            'fields': ('employeeID', 'National_id_number'),
-        }),
-        # Add more fieldsets as needed
-    )
-
-    # Customize how fields are displayed in the detail view
-    readonly_fields = ('gender')  # Fields that are read-only in the detail view
-
+    list_display = ('employeeID', 'social_security_number', 'gender', 'date_of_birth','National_id_number')
+    search_fields = ('employeeID', 'social_security_number','National_id_number')
+    list_filter = ('employeeID','National_id_number')
+    ordering = ('employeeID','social_security_number')
+    readonly_fields = ('gender')
 admin.site.register(EmployeeDetails, EmployeeDetailsAdmin)
--- hotelmanagement/admin.py
+++ hotelmanagement/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+def get_model_fields(self, model):
+        return [field.name for field in model._meta.fields]
+    # list_display = get_model_fields(YourModel)
 
 admin.site.register(Messages)
 admin.site.register(Ingredient)
@@ -18,3 +21,117 @@ admin.site.register(MenuImage)
 admin.site.register(User_type)
 admin.site.register(Table)
 admin.site.register(Cupon)
+
+
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Messages)
+    search_fields = get_model_fields(Messages)
+    list_filter = ('')
+    ordering = get_model_fields(Messages)
+    readonly_fields = ('')
+admin.site.register(Messages, MessagesAdmin)
+
+
+class CuponAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Cupon)
+    search_fields = get_model_fields(Cupon)
+    list_filter = ('')
+    ordering = get_model_fields(Cupon)
+    readonly_fields = ('')
+admin.site.register(Cupon, CuponAdmin)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Ingredient)
+    search_fields = get_model_fields(Ingredient)
+    list_filter = ('')
+    ordering = get_model_fields(Ingredient)
+    readonly_fields = ('')
+admin.site.register(Ingredient, IngredientAdmin)
+
+class MenuItemRatingAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(MenuItemRating)
+    search_fields = get_model_fields(MenuItemRating)
+    list_filter = ('')
+    ordering = get_model_fields(MenuItemRating)
+    readonly_fields = ('')
+admin.site.register(MenuItemRating, MenuItemRatingAdmin)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(CustomUser)
+    search_fields = get_model_fields(CustomUser)
+    list_filter = ('')
+    ordering = get_model_fields(CustomUser)
+    readonly_fields = ('')
+admin.site.register(CustomUser, CustomUserAdmin)
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Restaurant)
+    search_fields = get_model_fields(Restaurant)
+    list_filter = ('')
+    ordering = get_model_fields(Restaurant)
+    readonly_fields = ('')
+admin.site.register(Restaurant, RestaurantAdmin)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Review)
+    search_fields = get_model_fields(Review)
+    list_filter = ('')
+    ordering = get_model_fields(Review)
+    readonly_fields = ('')
+admin.site.register(Review, ReviewAdmin)
+
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(MenuCategory)
+    search_fields = get_model_fields(MenuCategory)
+    list_filter = ('')
+    ordering = get_model_fields(MenuCategory)
+    readonly_fields = ('')
+admin.site.register(MenuCategory, MenuCategoryAdmin)
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(MenuItem)
+    search_fields = get_model_fields(MenuItem)
+    list_filter = ('')
+    ordering = get_model_fields(MenuItem)
+    readonly_fields = ('')
+admin.site.register(MenuItem, MenuItemAdmin)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Employee)
+    search_fields = get_model_fields(Employee)
+    list_filter = ('')
+    ordering = get_model_fields(Employee)
+    readonly_fields = ('')
+admin.site.register(Employee, EmployeeAdmin)
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Payment)
+    search_fields = get_model_fields(Payment)
+    list_filter = ('')
+    ordering = get_model_fields(Payment)
+    readonly_fields = ('')
+admin.site.register(Payment, PaymentAdmin)
+
+class MenuImageAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(MenuImage)
+    search_fields = get_model_fields(MenuImage)
+    list_filter = ('')
+    ordering = get_model_fields(MenuImage)
+    readonly_fields = ('')
+admin.site.register(MenuImage, MenuImageAdmin)
+
+class User_typeAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(User_type)
+    search_fields = get_model_fields(User_type)
+    list_filter = ('')
+    ordering = get_model_fields(User_type)
+    readonly_fields = ('')
+admin.site.register(User_type, User_typeAdmin)
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = get_model_fields(Table)
+    search_fields = get_model_fields(Table)
+    list_filter = ('')
+    ordering = get_model_fields(Table)
+    readonly_fields = ('')
+admin.site.register(Table, TableAdmin)
