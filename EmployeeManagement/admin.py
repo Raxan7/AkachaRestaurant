a7568b7d2eff466e10ad5ba6e9fff13451283726
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-12-29 22:52:22 +0300

    Update admin.py

--- EmployeeManagement/admin.py
+++ EmployeeManagement/admin.py
@@ -3,19 +3,19 @@ from django.contrib import admin
 from .models import Departments
 
 class DepartmentsAdmin(admin.ModelAdmin):
-    list_display = ('field1', 'field2')  # Fields to be displayed in the list view
-    search_fields = ('field1', 'field2')  # Enable searching by these fields
-    list_filter = ('field2',)  # Add filters for these fields
-    ordering = ('field1',)  # Set the default sorting order
+    list_display = ('employeeID', 'social_security_number', 'gender', 'date_of_birth','National_id_number')  # Fields to be displayed in the list view
+    search_fields = ('employeeID', 'social_security_number','National_id_number')  # Enable searching by these fields
+    list_filter = ('employeeID',)  # Add filters for these fields
+    ordering = ('employeeID',)  # Set the default sorting order
 
     fieldsets = (
         ('Main Information', {
-            'fields': ('field1', 'field2'),
+            'fields': ('employeeID', 'National_id_number'),
         }),
         # Add more fieldsets as needed
     )
 
     # Customize how fields are displayed in the detail view
-    readonly_fields = ('field1',)  # Fields that are read-only in the detail view
+    readonly_fields = ('gender',)  # Fields that are read-only in the detail view
 
 admin.site.register(Departments, DepartmentsAdmin)
