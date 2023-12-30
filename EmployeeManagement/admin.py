cc671e2411941199c82e614db66c570647381cc8
atmmtawa <134715890+atmmtawa@users.noreply.github.com>
2023-12-30 11:03:37 +0300

    Update admin.py

--- EmployeeManagement/admin.py
+++ EmployeeManagement/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import EmployeeDetails
 
 class EmployeeDetailsAdmin(admin.ModelAdmin):
-    list_display = ('employeeID', 'social_security_number', 'gender', 'date_of_birth','National_id_number')
+    list_display = ('user', 'employeeID', 'social_security_number', 'gender', 'date_of_birth','National_id_number')
     search_fields = ('employeeID', 'social_security_number','National_id_number')
     list_filter = ('employeeID','National_id_number')
     ordering = ('employeeID','social_security_number')
